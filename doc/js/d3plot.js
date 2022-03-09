0313d6d082f138525687544b9c7dc5b722bc8809
tom <tom.young@chem.ox.ac.uk>
2022-03-09 11:16:36 +0000

    Tidy js

--- doc/js/d3plot.js
+++ doc/js/d3plot.js
@@ -1,77 +1,58 @@
-require.config({paths: {d3: "https://d3js.org/d3.v3.min"}});
-require(["d3"], function(d3) {
+const width = 400, height = 400;
+const color = d3.scale.category10();
 
-    // Parameter declaration, the height and width of our viz.
-    var width = 300,
-        height = 300;
+let force = d3.layout.force()
+    .charge(-120)
+    .linkDistance(70)
+    .size([width, height]);
 
-    // Colour scale for node colours.
-    var color = d3.scale.category10();
+let svg = d3.select("#d3-network-container").select("svg");
+if (svg.empty()) {
+    svg = d3.select("#d3-network-container").append("svg")
+                .attr("width", width)
+                .attr("height", height)
+                .attr("preserveAspectRatio", "xMidYMid meet");
+}
 
-    // We create a force-directed dynamic graph layout.
-    // D3 has number of layouts - refer to the documentation.
-    var force = d3.layout.force()
-        .charge(-120)
-        .linkDistance(30)
-        .size([width, height]);
+d3.json("assets/network.json", function(error, graph) {
 
-    // We select the < div> we created earlier and add an
-    // SVG = Scalable Vector Graphics
-    var svg = d3.select("#d3-container").select("svg")
-    if (svg.empty()) {
-        svg = d3.select("#d3-container").append("svg")
-                    .attr("width", width)
-                    .attr("height", height);
-    }
+    force.nodes(graph.nodes)
+        .links(graph.links)
+        .start();
 
-    // We load the JSON network file.
-    d3.json("graph.json", function(error, graph) {
-        // Within this block, the network has been loaded
-        // and stored in the 'graph' object.
+    let link = svg.selectAll(".link")  // Create edges
+        .data(graph.links)
+        .enter().append("line")
+        .attr("class", "link")
+        .attr("stroke-width", 2);
 
-        // We load the nodes and links into the force-directed
-        // graph and initialise the dynamics.
-        force.nodes(graph.nodes)
-            .links(graph.links)
-            .start();
+    let node = svg.selectAll(".node")  // Create nodes
+        .data(graph.nodes)
+        .enter().append("circle")
+        .attr("class", "node")
+        .attr("r", function (n) {     // Radius
+            if (n.type === "major"){
+                return 15;
+            }
+            return 10;
+        })               // radius
+        .style("fill", function (n) {
+            // We colour the node depending on the degree.
+            return color(n.degree);
+        })
+        .call(force.drag);
 
-        // We create a < line> SVG element for each link
-        // in the graph.
-        var link = svg.selectAll(".link")
-            .data(graph.links)
-            .enter().append("line")
-            .attr("class", "link");
+    // The label each node its name from the networkx graph.
+    node.append("title")
+        .text(function(d) { return d.name; });
 
-        // We create a < circle> SVG element for each node
-        // in the graph, and we specify a few attributes.
-        var node = svg.selectAll(".node")
-            .data(graph.nodes)
-            .enter().append("circle")
-            .attr("class", "node")
-            .attr("r", 5)  // radius
-            .style("fill", function(d) {
-                // We colour the node depending on the degree.
-                return color(d.degree);
-            })
-            .call(force.drag);
+    force.on("tick", function() {
+        link.attr("x1", function(d) { return d.source.x; })
+            .attr("y1", function(d) { return d.source.y; })
+            .attr("x2", function(d) { return d.target.x; })
+            .attr("y2", function(d) { return d.target.y; });
 
-        // The label each node its node number from the networkx graph.
-        node.append("title")
-            .text(function(d) { return d.id; });
-
-
-
-        // We bind the positions of the SVG elements
-        // to the positions of the dynamic force-directed graph,
-        // at each time step.
-        force.on("tick", function() {
-            link.attr("x1", function(d) { return d.source.x; })
-                .attr("y1", function(d) { return d.source.y; })
-                .attr("x2", function(d) { return d.target.x; })
-                .attr("y2", function(d) { return d.target.y; });
-
-            node.attr("cx", function(d) { return d.x; })
-                .attr("cy", function(d) { return d.y; });
-        });
+        node.attr("cx", function(d) { return d.x; })
+            .attr("cy", function(d) { return d.y; });
     });
 });
