6e55587788df44132a23a8a1c14b067a30fd0d6a
tom <tom.young@chem.ox.ac.uk>
2022-03-10 16:40:08 +0000

    Node highlighting

--- doc/js/d3plot.js
+++ doc/js/d3plot.js
@@ -8,30 +8,38 @@ let force = d3.layout.force()
     })
     .size([width, height]);
 
+let svg = d3.select("#d3-network-container").select("svg");
+
+if (svg.empty()) { svg = create_svg(); }
+
 let tooltip = d3.select("#d3-network-container")
     .append("div")
     .attr("class", "tooltip")
     .style("position", "relative");
 
-let svg = d3.select("#d3-network-container").select("svg");
+d3.json("assets/network.json", function(error, network) {
 
-if (svg.empty()) {
-    svg = d3.select("#d3-network-container").append("svg")
+    // network parameter is built from the json...
+    force.nodes(network.nodes).links(network.links).start();
+
+    let links = create_links(network);
+    let nodes = create_nodes(network);
+    force.on("tick", function() { update_positions(nodes, links); });
+
+    network.a_node_is_highlighted = false;
+    network.link_dict = create_network_link_dict(network);
+
+    nodes.on('click', function (d) {
+        highlight_neighbours(d, nodes, links, network);
+    });
+});
+
+function create_svg(){
+    return d3.select("#d3-network-container").append("svg")
                 .attr("width", width)
                 .attr("height", height)
                 .attr("preserveAspectRatio", "xMidYMid meet");
 }
-
-d3.json("assets/network.json", function(error, network) {
-
-    force.nodes(network.nodes)
-        .links(network.links)
-        .start();
-
-    let links = create_links(network);
-    let nodes = create_nodes(network);
-    force.on("tick", function() { update_network_position(links, nodes);});
-});
 
 function create_links(network){
     // Create edges in the network
@@ -55,10 +63,7 @@ function create_nodes(network){
             if (d.name === "timeseries") { return 20; }
             return 10;
         })
-        .style("opacity", function (d){
-            if (d.type === "minor"){ return 0.7; }
-            return 1.0;
-        })
+        .style("opacity", default_node_opacity)
         .style("fill", function (n) {
             // We colour the node depending on the degree.
             return d3.rgb(n.color);
@@ -67,9 +72,13 @@ function create_nodes(network){
             show_tooltip(n);
             if (n.name === "timeseries"){ show_time_series();}
         })
-        .on("mouseout", function (){ tooltip.style("opacity", 0);}
-        )
+        .on("mouseout", function (){ tooltip.style("opacity", 0);})
         .call(force.drag);
+}
+
+function default_node_opacity(node){
+    if (node.type === "minor"){ return 0.7; }
+    return 1.0;
 }
 
 function show_tooltip(n){
@@ -91,7 +100,7 @@ function show_time_series(){
     collapsable.show();
 }
 
-function update_network_position(links, nodes){
+function update_positions(nodes, links){
     links.attr("x1", function(d) { return d.source.x; })
          .attr("y1", function(d) { return d.source.y; })
          .attr("x2", function(d) { return d.target.x; })
@@ -101,3 +110,49 @@ function update_network_position(links, nodes){
         .attr("cy", function(d) { return d.y; });
 }
 
+function highlight_neighbours(o, nodes, links, network){
+    // Highlight a node and its neighbours based on the links, update the
+    // opacities and link widths
+
+    function are_neighbours(a, b) {
+        return network.link_dict[a.index + "," + b.index];
+    }
+
+    if (!network.a_node_is_highlighted) {
+
+        nodes.style("opacity", function (d) {
+            if (d.id === o.id){ return 1.0; }
+            if (o.type === "major"){ return 0.3; }
+            return (are_neighbours(d, o) || are_neighbours(o, d)) ? 0.8 : 0.3;
+        });
+
+        links.style("opacity", function (d) {
+            return (o.id===d.source.id ||  o.id===d.target.id) ? 0.8 : 0.3;}
+        )
+            .style("stroke-width", function (d) {
+            return (o.id===d.source.id ||  o.id===d.target.id) ? 3 : 2;
+        });
+
+        network.a_node_is_highlighted = true;
+    } else {
+        nodes.style("opacity", default_node_opacity);
+        links.style("opacity", 1).style("stroke-width", 2);
+        network.a_node_is_highlighted = false;
+    }
+}
+
+function create_network_link_dict(network){
+    // Create a dictionary of bits for whether two nodes in the network are linked
+
+    let linked_dict = {};
+
+    for (let i = 0; i < network.nodes.length; i++) {
+        linked_dict[i + "," + i] = true;
+    }
+
+    network.links.forEach(function (d) {
+        linked_dict[d.source.index + "," + d.target.index] = true;
+    });
+
+    return linked_dict;
+}
