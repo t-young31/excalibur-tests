b04c9a6ca3fa685af33e3e4584e521cc87faffe9
tom <tom.young@chem.ox.ac.uk>
2022-03-13 14:49:22 +0000

    Add some results

--- apps/gromacs/reframe_gromacs_strong_scaling.py
+++ apps/gromacs/reframe_gromacs_strong_scaling.py
@@ -3,9 +3,11 @@ Strong scaling of GROMACS. Uses the benchmark simulation from
 https://www.hecbiosim.ac.uk/access-hpc/benchmarks
 """
 import os
+import math
 import reframe as rfm
 import reframe.utility.sanity as sn
-from reframe.core.decorators import run_before
+from abc import abstractmethod
+from reframe.core.decorators import run_before, run_after
 
 this_dir = os.path.dirname(__file__)
 
@@ -38,6 +40,7 @@ def spack_env_dir(hostname):
 
 
 class GROMACSBenchmark(rfm.RegressionTest):
+    """Base class for a GROMACS benchmark"""
 
     valid_systems = ['*']
     valid_prog_environs = ['*']
@@ -54,36 +57,48 @@ class GROMACSBenchmark(rfm.RegressionTest):
         '*': {'ns/day': (1, None, None, 's')}
     }
 
-    def __init__(self,
-                 num_cores:       int,
-                 num_omp_threads: int = 4
-                 ):
+    @property
+    @abstractmethod
+    def _total_n_cores(self) -> int:
+        """Total number of cores to use"""
+
+    @property
+    @abstractmethod
+    def _num_omp_threads(self) -> int:
         """
-        Base class for a GROMACS benchmark
-
-        -----------------------------------------------------------------------
-        Args:
-            num_cores: Total number of cores to use
-
-            num_omp_threads: Number of omp threads to use. For example,
-                             num_cores=8, num_omp_threads=4 -> 2 MPI tasks each
-                             using 4 OMP threads
+        Number of omp threads to use. For example, num_cores=8,
+        num_omp_threads=4 -> 2 MPI tasks each using 4 OMP threads
         """
-        super().__init__()
 
-        self.num_tasks = max(num_cores//num_omp_threads, 1)
-        self.num_tasks_per_node = self.num_tasks
+    @run_before('compile')
+    def setup_build_system(self):
+        """Set a specific version of GROMACS to use"""
+        self.build_system.specs = ['gromacs@2019%gcc@9.3.0^openmpi@4.1.1']
+        self.build_system.environment = spack_env_dir(self.current_system.name)
 
-        # print(dir(self.current_partition))
-        # print(self.current_partition.num_cores)
-        # TODO: How to get the maximum number of cores on this system
+    @run_after('setup')
+    def set_attributes_after_setup(self):
 
-        if num_cores // num_omp_threads == 0:
+        self.num_tasks = max(self._total_n_cores // self._num_omp_threads, 1)
+
+        try:
+            cpus_per_node = self._current_partition.processor.num_cpus
+            num_nodes = math.ceil(self.num_tasks / cpus_per_node)
+
+        except AttributeError:
+            print('WARNING: Failed to determine the number of nodes required '
+                  'defaulting to 1')
+            num_nodes = 1
+
+        self.num_tasks_per_node = math.ceil(self.num_tasks / num_nodes)
+
+        if self._total_n_cores // self._num_omp_threads == 0:
             print('WARNING: Had fewer total number of cores than the default '
-                  f'number of OMP threads, using {num_cores} OMP threads')
-            self.num_cpus_per_task = num_cores
+                  f'number of OMP threads, using {self._total_n_cores} OMP '
+                  f'threads')
+            self.num_cpus_per_task = self._total_n_cores
         else:
-            self.num_cpus_per_task = num_omp_threads
+            self.num_cpus_per_task = self._num_omp_threads
 
         self.variables = {
             'OMP_NUM_THREADS': f'{self.num_cpus_per_task}',
@@ -93,11 +108,7 @@ class GROMACSBenchmark(rfm.RegressionTest):
             'mpi': {'num_slots': self.num_tasks * self.num_cpus_per_task}
         }
 
-    @run_before('compile')
-    def setup_build_system(self):
-        """Set a specific version of GROMACS to use"""
-        self.build_system.specs = ['gromacs@2019%gcc@9.3.0^openmpi@4.1.1']
-        self.build_system.environment = spack_env_dir(self.current_system.name)
+        self.tags |= {f'num_procs={self.num_tasks}', f'num_nodes={num_nodes}'}
 
     @run_before('sanity')
     def set_sanity_patterns(self):
@@ -117,7 +128,14 @@ class GROMACSBenchmark(rfm.RegressionTest):
 
 
 @rfm.simple_test
-class TestCase(GROMACSBenchmark):
+class StrongScalingBenchmark(GROMACSBenchmark):
 
-    def __init__(self):
-        super().__init__(num_cores=16)
+    variant = parameter([4*i for i in range(2, 7)])
+
+    @property
+    def _total_n_cores(self) -> int:
+        return self.variant
+
+    @property
+    def _num_omp_threads(self) -> int:
+        return 4
